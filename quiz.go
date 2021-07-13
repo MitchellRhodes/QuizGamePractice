7b8701385ea6a65c3ca4b71e4cc15e17b739207a
Mitchell Rhodes <M.Rhodes87@comcast.net>
2021-07-13 19:02:00 -0400

    Added headers to csv. Added error handling to quizReader function, and isolated questions

--- quiz.go
+++ quiz.go
@@ -3,25 +3,54 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"io"
+	"log"
 	"os"
 )
 
+type Quiz struct {
+	Question string `csv:"Question"`
+	Answer   string `csv:"Answer"`
+}
+
 func main() {
 	quizReader()
+	// quiz := Quiz{
+	// 	Question: records[0], //outputs question row
+	// 	Answer:   records[1], //outputs answer row
+	// }
 }
 
 func quizReader() {
 	//open the file
 	quizFile, err := os.Open("./questionsAndAnswers.csv")
 	if err != nil {
-		fmt.Println("Error occured ::", err)
+		panic(err)
 	}
 	//initialize reader
 	reader := csv.NewReader(quizFile)
 
-	//read all file
-	quiz, _ := reader.ReadAll()
+	//discarding the header
+	_, err = reader.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	//print file
-	fmt.Println(quiz)
+	//read all of the file
+	for {
+		records, err := reader.Read()
+
+		//EOF is the error returned by Read when no more input
+		//is available. This breaks at end of read
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		//print file
+		fmt.Println(records[0])
+	}
+
 }
