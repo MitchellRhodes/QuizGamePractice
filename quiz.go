27ef89094102dcf8b96a45635f095ea9977c4daf
Mitchell Rhodes <M.Rhodes87@comcast.net>
2021-07-14 23:42:32 -0400

    Added timer. Need to fix user being able to finish last question when out of time

--- quiz.go
+++ quiz.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 type Quiz struct {
@@ -17,12 +18,15 @@ type Quiz struct {
 func main() {
 	csvFilename := flag.String("csv", "./quizzes/addition.csv",
 		"A csv in the format of 'Question,Answer'. ./quizzes/subtraction.csv and ./quizzes/multiplication.csv are also available")
+
+	timeLimit := flag.Int("Timelimit", 30, "Timelimit formatted in seconds.")
 	flag.Parse()
-	quizReader(*csvFilename)
+
+	quizReader(*csvFilename, *timeLimit)
 
 }
 
-func quizReader(fileName string) {
+func quizReader(fileName string, timeLimit int) {
 	//open the file
 	quizFile, err := os.Open(fileName)
 	if err != nil {
@@ -47,16 +51,27 @@ func quizReader(fileName string) {
 	quiz := parseRows(rows)
 	var numberCorrect int = 0
 
+	timer := time.NewTimer(time.Duration(timeLimit) * time.Second)
+
 	//loop through the struct to output questions
 	//i+1 is so that we skip the 0 index, which is a header
 	for i, problem := range quiz {
-		fmt.Printf("Solve #%d: %s = \n", i+1, problem.Question)
 
-		var userAnswer string
-		fmt.Scanf("%s\n", &userAnswer)
+		select {
 
-		if userAnswer == problem.Answer {
-			numberCorrect++
+		case <-timer.C:
+			fmt.Printf("You got %d out of %d correct. \n", numberCorrect, len(quiz))
+			return
+
+		default:
+			fmt.Printf("Solve #%d: %s = \n", i+1, problem.Question)
+
+			var userAnswer string
+			fmt.Scanf("%s\n", &userAnswer)
+
+			if userAnswer == problem.Answer {
+				numberCorrect++
+			}
 		}
 	}
 
